3eb6bdd6a832235349511ea139cdd7973faa254e
michaelstrupler <michael.strupler@gmx.ch>
2024-06-19 12:06:12 +0200

    changed error in "Collar" label

--- RespChamberProc/main/functions/ChunkPlots.R
+++ RespChamberProc/main/functions/ChunkPlots.R
@@ -3,7 +3,7 @@ chunk_labels <- function(dsChunk, gas_column, y_multiplier) {
   dsChunk %>%
     group_by(iChunk) %>%
     summarise(
-      Collar = first(Collar),  # Use the Collar value for each iChunk
+      collar = first(collar),  # Use the Collar value for each iChunk
       x = max(TIMESTAMP),      # Use max TIMESTAMP for x position
       y = max({{ gas_column }}) * y_multiplier  # Use slightly above max gas value for y position
     )
@@ -14,6 +14,6 @@ chunk_plot <- function(dsChunk, gas_column, labels, y_col) {
   ggplot(dsChunk, aes(x = TIMESTAMP, y = {{ gas_column }})) +
     geom_point(pch='.') +
     facet_wrap(~ iChunk, scales = "free") +
-    geom_text(data = labels, aes(x = x, y = y, label = paste("Collar", Collar, sep = " ")),
+    geom_text(data = labels, aes(x = x, y = y, label = paste("Collar", collar, sep = " ")),
               hjust = 1, vjust = 1, inherit.aes = FALSE)
 }
